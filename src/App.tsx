064633e134166cfa2995825b99e90ca815866b2c
MikiWojak <mikizarowa8@gmail.com>
2023-08-15 13:31:01 +0200

    Add logic to input

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import './assets/styles.css'
 import InputField from './components/InputField'
 
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("")
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
 
-      <InputField />
+      <InputField
+        todo={todo}
+        setTodo={setTodo}
+      />
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
-const InputField: React.FC = () => {
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
     return (
         <form className="input">
             <input 
                 type="input"
                 placeholder="Enter a task"
                 className="input__box"
+                value={todo}
+                onChange={
+                    (e) => setTodo(e.target.value)
+                }
             />
 
             <button
