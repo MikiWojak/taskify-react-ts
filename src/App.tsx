c1b5916f80b5e5cdc179f5c8420ac21c901e8fd6
Mikołaj <53621297+MikiWojak@users.noreply.github.com>
2023-08-19 10:50:06 +0200

    Todo List - basic (#2)
    - Basic Todo list
    - Add, edit, delete, mark as done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import './App.css';
+import './assets/styles.css'
+import TodoList from './components/TodoList';
+import InputField from './components/InputField'
+
+import { Todo } from './model'
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>('')
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault()
+
+    if (todo) {
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          todo,
+          isDone: false
+        }
+      ])
+
+      setTodo('')
+    }
+  }
+
   return (
     <div className="App">
-      Hello world
+      <span className="heading">Taskify</span>
+
+      <InputField
+        todo={todo}
+        setTodo={setTodo}
+        handleAdd={handleAdd}
+      />
+
+      <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
